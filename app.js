76d4adcbdbe0949ed3e037bbbc7dab5e0cda800e
Shahzaib0000 <shahzaibawan085@gmail.com>
2021-07-01 09:21:06 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,83 @@
+// ********** Objects *********
+
+// ********** Question No 01 *******
+
+// var itemsArray = [
+//     { name: "juice", price: "50", quantity: "3" },
+//     { name: "cookie", price: "30", quantity: "9" },
+//     { name: "shirt", price: "880", quantity: "1" },
+//     { name: "pen", price: "100", quantity: "2" }
+// ];
+
+// // console.log("Total Price of Item 01 is :" + " " + itemsArray[0].price * itemsArray[0].quantity )
+// // console.log( "Total Price of Item 02 is :" + " " + itemsArray[1].price * itemsArray[1].quantity )
+// // console.log( "Total Price of Item 03 is :" + " " +  itemsArray[2].price * itemsArray[2].quantity )
+// // console.log("Total Price of Item 04 is :" +  " " + itemsArray[3].price * itemsArray[3].quantity )
+
+// for (var i=0; i<itemsArray.length; i++){
+//     totalPrice = itemsArray[i].price * itemsArray[i].quantity
+//     console.log(totalPrice)
+//     // totalbill =totalPrice +  itemsArray[i].price * itemsArray[i].quantity
+//     // console.log(totalbill)
+// }
+// // console.log("Total Price of all Item is :" +  " " + itemsArray[3].price * itemsArray[3].quantity )
+
+
+//  ************ Question No 02 ********
+
+// var info = {
+//     name: "awan",
+//     age: 21,
+//     gender: "male",
+//     email: "shahzaibawawn@gmail.com",
+//     password: 0000,
+//     city: "Karachi",
+//     country: "Pakistan"}
+
+// var propertyChecking = "age" && "city" in info;
+// console.log(propertyChecking)
+
+// var propCheck = "firstNAme " && "lastName" in info;
+// console.log(propCheck)
+
+// ******* Question No 03 ************
+
+// function Person(name,age,gender,mail,phone){
+//     this.name = name,
+//     this.age = age,
+//     this.gender = gender,
+//     this.mail = mail,
+//     this.phone = phone    
+// }
+
+// var Person1 = new Person("Shahzaib", 21 , "male", "shakfda@gmail.com" , 343525)
+// console.log(Person1)
+// var Person1 = new Person("Junaid", 23 , "male", "johny@gmail.com" ,98765432)
+// console.log(Person1)
+
+// ************ Question No 04 ************
+
+// function Person(name,gender,address,education,profession){
+//     this.name = name,
+//     this.gender = gender,
+//     this.address = address,
+//     this.education = education,
+//     this.profession = profession    
+// }
+
+// var Person1 = new Person("Shahzaib", "male", "533 block-3 Nazimabad " , "Intermediate", "Student")
+// console.log(Person1)
+// var Person1 = new Person("Awan", "Male", "532 block-3 Nazimabad " , "Matric", "Student")
+// console.log(Person1)
+
+
+// var Person1 = new Person("Gulshan", "Female", "533 block-3 Nazimabad " , "MAsters", "Teacher")
+// console.log(Person1)
+// var Person1 = new Person("Siraj", "Male", "200 block-3 Nazimabad " , "Intermediate", "Businessman")
+// console.log(Person1)
+
+
+// var Person1 = new Person("Javed", "Male", "001 Baldia" , "MAtric", "Carpenter")
+// console.log(Person1)
+// var Person1 = new Person("Ghous", "Male", "533 block-3 North Nazimabad " , "Intermediate", "Web Developer")
+// console.log(Person1)
